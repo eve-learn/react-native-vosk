e4f31b3eea14869babac8603afd6f974d1adbb19
vidman22 <29338715+vidman22@users.noreply.github.com>
2024-04-28 16:13:07 -0700

    new swift

--- ios/VoskModel.swift
+++ ios/VoskModel.swift
@@ -13,19 +13,44 @@ public final class VoskModel {
     var model : OpaquePointer!
     var spkModel : OpaquePointer!
     
+    // init(name: String) throws {
+        
+    //     // Set to -1 to disable logs
+    //     vosk_set_log_level(0);
+        
+    //     let appBundle = Bundle(for: Self.self)
+        
+    //     // Load model from main app bundle
+    //     if let resourcePath = Bundle.main.resourcePath {
+    //         let modelPath = resourcePath + "/" + name
+    //         model = vosk_model_new(modelPath)
+    //     }
+
+    //     // Get the URL to the spk model inside this pod
+    //     if let spkModelPath = appBundle.path(forResource: "vosk-model-spk-0.4", ofType: nil) {
+    //         spkModel = vosk_spk_model_new(spkModelPath)
+    //     }
+    // }
+    
+//    func getDocumentsDirectory() -> URL {
+//        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+//        let documentsDirectory = paths[0]
+//        return documentsDirectory
+//    }
+
     init(name: String) throws {
         
         // Set to -1 to disable logs
         vosk_set_log_level(0);
         
+        let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let modelURL = documentsDirectory.appendingPathComponent(name)
+        
+        // Load model from documents directory
+        model = vosk_model_new(modelURL.path)
+        
         let appBundle = Bundle(for: Self.self)
         
-        // Load model from main app bundle
-        if let resourcePath = Bundle.main.resourcePath {
-            let modelPath = resourcePath + "/" + name
-            model = vosk_model_new(modelPath)
-        }
-
         // Get the URL to the spk model inside this pod
         if let spkModelPath = appBundle.path(forResource: "vosk-model-spk-0.4", ofType: nil) {
             spkModel = vosk_spk_model_new(spkModelPath)
